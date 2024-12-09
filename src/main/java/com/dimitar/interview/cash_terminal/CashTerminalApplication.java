a7085ccd71a4f3e356196157fbd84d5fb2124a12
Dimitar Naydenov <naidim@abv.bg>
2024-12-09 15:31:55 +0200

    Initialized intelliJ project

---
+++ src/main/java/com/dimitar/interview/cash_terminal/CashTerminalApplication.java
@@ -0,0 +1,13 @@
+package com.dimitar.interview.cash_terminal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashTerminalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashTerminalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dimitar/interview/cash_terminal/CashTerminalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimitar.interview.cash_terminal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashTerminalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
